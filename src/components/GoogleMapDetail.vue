c9192f74e375dccdf41460b095d5fffd2593bfef
ChristianHS23 <christian.sihotang23@gmail.com>
2019-02-25 12:00:46 +0700

    Update Detail

---
+++ src/components/GoogleMapDetail.vue
@@ -0,0 +1,20 @@
+<template>
+<GmapMap
+  :center="{lat:-6.2607, lng:106.7816}"
+  :zoom="20"
+  map-type-id="terrain"
+  style="width: 500px; height: 300px"
+>
+ <GmapMarker
+    :position="{lat:-6.2607, lng:106.7816}"
+    :clickable="true"
+    :draggable="true"
+    @click="center = {lat:-6.2607, lng:106.7816}"
+  />
+
+</GmapMap>
+</template>
+<script>
+export default {
+
+}
